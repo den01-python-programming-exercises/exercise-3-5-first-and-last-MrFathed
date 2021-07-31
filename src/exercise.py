2c18005e49c2760d3a5ebf2b7fcf71a97b1c3ea1
Justin Turner <jmturner638@gmail.com>
2021-07-31 01:32:22 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    names = []
+
+    while True:
+        name = input()
+
+        if name == '':
+            break
+
+        names.append(name)
+    
+    print(names[0])
+    print(names[-1])
 
 if __name__ == '__main__':
     main()
